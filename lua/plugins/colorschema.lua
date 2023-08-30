1c7cbe43bb690f94d194034def2222d0c058541e
yuanchengxiang <yuanchengxiang@bytedance.com>
2023-08-30 21:34:49 +0800

    init:初始化nvim

---
+++ lua/plugins/colorschema.lua
@@ -0,0 +1,13 @@
+-- https://vimcolorschemes.com/ 可参考的 nvim 背景
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox",
+    },
+  },
+}
