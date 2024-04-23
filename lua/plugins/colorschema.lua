d9de2d434908350fd80897e38cf0838b54e756d3
Eminem-x <1773997586@qq.com>
2024-04-23 19:43:50 +0300

    feat: 切换主题适配 gitui

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,30 +1,36 @@
 -- https://vimcolorschemes.com/ 可参考的 nvim 背景
 return {
-    -- add gruvbox
-    { "ellisonleao/gruvbox.nvim" },
-    -- add catppuccin
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  -- add catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      no_italic = true,
     },
-    -- add onedark
-    -- { "joshdick/onedark.vim" },
-    {
-        "folke/tokyonight.nvim",
-        lazy = true,
-        opts = { style = "storm" },
-    },
-    {
-        "navarasu/onedark.nvim",
-    },
+  },
+  -- add onedark
+  -- { "joshdick/onedark.vim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "storm" },
+  },
+  {
+    "navarasu/onedark.nvim",
+  },
+  {
+    "Mofiqul/vscode.nvim",
+  },
 
-    -- Configure LazyVim to load gruvbox
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            -- colorscheme = "catppuccin",
-            colorscheme = "onedark",
-        },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin",
+      colorscheme = "catppuccin",
     },
+  },
 }
