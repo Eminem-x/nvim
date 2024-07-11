0d7a692a6a03edc5843ba50f2b284d65fcb2ad25
Eminem-x <1773997586@qq.com>
2024-07-11 12:02:06 +0800

    feat: upgrade some plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -151,4 +151,26 @@ return {
     end,
   },
   { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        title = "Database",
+        ft = "dbui",
+        pinned = true,
+        width = 10,
+        open = function()
+          vim.cmd("DBUI")
+        end,
+      })
+
+      opts.bottom = opts.bottom or {}
+      table.insert(opts.bottom, {
+        title = "DB Query Result",
+        ft = "dbout",
+      })
+    end,
+  },
 }
